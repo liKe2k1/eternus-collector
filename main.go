7cdd742a086d35aed6138e4cb285efa8d60efe83
Ehrig, Tobias <tobias.ehrig@hermesworld.com>
2018-02-16 14:44:32 +0100

    Change behavior of connecting to storage system

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"os"
 	"time"
+
 	"gopkg.in/gcfg.v1"
 	"github.com/like2k1/eternus-collector/pkg/input"
 	"github.com/like2k1/eternus-collector/pkg/output"
+	"github.com/like2k1/eternus-collector/pkg/remote"
 	"github.com/like2k1/eternus-collector/pkg/types"
 )
 
